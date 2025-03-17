7153f62d8dd50b56a1cdb5ae067e24921aaadfb9
kathsup <katherinencarvallo22@gmail.com>
2025-03-16 21:33:03 -0600

    cambio

--- core/src/main/java/com/rope/Game/Nivel5.java
+++ core/src/main/java/com/rope/Game/Nivel5.java
@@ -541,13 +541,13 @@ public class Nivel5 extends NivelBase implements Screen {
     @Override
     public void verificarCondicionesPerdida() {
         if (ballBody != null && (ballBody.getPosition().y < -15 || ballBody.getPosition().y > 18)) { 
-            perderNivel();
+           perderNivel();
         }
     }
 
     @Override
     protected void reiniciarNivel() {
          System.out.println("Reiniciando Nivel 5...");
-        mostrarCuadroVictoria();// Recargar la pantalla del nivel 1
+        mostrarCuadroDerrota();// Recargar la pantalla del nivel 1
     }
 }
